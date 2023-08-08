a16693fc0c83b11ef02fc5b784b63d725dfa019b
SelmaPalmquist <pase22pc@student.ju.se>
2023-08-09 00:49:01 +0200

    java updates

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+ window.addEventListener('load', () =>{
     let taskbar = document.querySelector('#new-task-form');
     let actualtask = document.querySelector('#new-task-input');
     let list = document.querySelector('#tasks');
+    
+    taskbar.addEventListener('submit', (e) =>{
+        e.preventDefault();
+    
+        let task = actualtask.value;
+    
+        if (!task){
+            alert("Please write a task.");
+            return;
+        }
+    })
+        
+    })
